52d9aeabe2f7728a89d677ab75a307abccb55799
haduc25 <haducvcvb@gmail.com>
2023-04-21 22:52:07 +0700

    skills have basic and css need responsive

--- Projects/website_about_me/Portfolio-Website/myself/src/App.js
+++ Projects/website_about_me/Portfolio-Website/myself/src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills';
 
 function App() {
     return (
         <>
             <Header />
 
-            <main className="main">
+            <main className="main" style={{ paddingBottom: '500px' }}>
                 <Home />
                 <About />
+                <Skills />
             </main>
         </>
     );
