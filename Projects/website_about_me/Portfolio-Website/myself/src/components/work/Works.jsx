180b69131a97edcfcd2140c6845e7d89d1d16b35
haduc25 <haducvcvb@gmail.com>
2023-04-24 17:07:19 +0700

    component Data, WorkItems, Works is created and have base

---
+++ Projects/website_about_me/Portfolio-Website/myself/src/components/work/Works.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { projectsData, projectsNav } from './Data';
+import WorkItems from './WorkItems';
+
+const Works = () => {
+    return (
+        <div>
+            <div className="work__filters">
+                {projectsNav.map((item, index) => (
+                    <span className="work__item" key={index}>
+                        {item.name}
+                    </span>
+                ))}
+            </div>
+
+            <div className="work__container container grid">
+                {projectsData.map((item) => (
+                    <WorkItems item={item} key={item.id} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Works;
