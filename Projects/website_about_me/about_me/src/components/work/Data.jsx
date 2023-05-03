3944aa43606fddc9f2bf1ea8c8540cf7415216c0
Ha Duc <haducvcvb@gmail.com>
2023-05-03 19:13:51 +0700

    about_me-handle directory

--- Projects/website_about_me/about_me/src/components/work/Data.jsx
+++ Projects/website_about_me/about_me/src/components/work/Data.jsx
@@ -1,47 +1,59 @@
-import Work1 from '../../assets/works/worklist1/work1.jpg';
-import Work2 from '../../assets/works/worklist2/work2.jpg';
-import Work3 from '../../assets/works/worklist3/work3.jpg';
-import Work4 from '../../assets/works/worklist4/work4.jpg';
-import Work5 from '../../assets/works/worklist5/5.png';
-import Work6 from '../../assets/works/worklist5/1.png';
-import Work6Slider from '../../assets/works/worklist5/project.gif';
+import Work1 from '../../assets/works/work1.jpg';
+import Work2 from '../../assets/works/work2.jpg';
+import Work3 from '../../assets/works/work3.jpg';
+import Work4 from '../../assets/works/work4.jpg';
+import Work5 from '../../assets/works/work4.jpg';
+import Work6 from '../../assets/works/web_gtsp_preview.png';
+import Work6Slider from '../../assets/works/web_gtsp_slider.gif';
 
 export const projectsData = [
     {
         id: 1,
         image: Work1,
+        imgSlider: Work1,
         title: 'Web design',
         category: 'web',
     },
     {
         id: 2,
         image: Work2,
+        imgSlider: Work1,
         title: 'App movil',
         category: 'app',
     },
     {
         id: 3,
         image: Work3,
+        imgSlider: Work1,
         title: 'Brand design',
         category: 'design',
     },
     {
         id: 4,
         image: Work4,
-        title: 'App movil',
+        imgSlider: Work1,
+        title: 'App Music - JS',
         category: 'app',
     },
     {
         id: 5,
         image: Work5,
-        title: 'Web design',
+        imgSlider: Work1,
+        title: 'Website Tiktok - ReactJS',
         category: 'web',
     },
     {
         id: 6,
         image: Work6,
         imgSlider: Work6Slider,
-        title: 'Website giới thiệu sản phẩm',
+        title: 'Website Shopee - HTML',
+        category: 'web',
+    },
+    {
+        id: 7,
+        image: Work6,
+        imgSlider: Work6Slider,
+        title: 'Website giới thiệu sản phẩm - PHP',
         category: 'web',
     },
 ];
