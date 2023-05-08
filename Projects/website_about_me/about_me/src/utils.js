1018a7854d5e568784642caa394c739b480cb01d
Ha Duc <haducvcvb@gmail.com>
2023-05-08 09:18:28 +0700

    about-me_upgrade #3

--- Projects/website_about_me/about_me/src/utils.js
+++ Projects/website_about_me/about_me/src/utils.js
@@ -5,7 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // handle when call the notify
-export const handleNotify = (e, { haveImage = false, title, content, type = 'toast', ...props }) => {
+export const handleNotify = (e, { haveImage = false, title, content, toastType = 'error', ...props }) => {
     // bcs dont have pages preview so i'll show them the message (toast message)
 
     // first remove preventDefault() of a card
@@ -14,24 +14,56 @@ export const handleNotify = (e, { haveImage = false, title, content, type = 'toa
     // handle for dark theme & light theme
     const isDarkTheme = getComputedStyle(document.body).getPropertyValue('--body-color') === '#202124';
 
-    const toastFuncs = haveImage && props.srcImg ? toast[type] : toast;
+    // handle for toast types
 
-    toast.info(
-        <div>
-            {haveImage && <img src={props.srcImg} alt="Oops!!!" style={{ maxWidth: '100%', maxHeight: '250px' }} />}
-            <h3>{title}</h3>
-            <p>{content}</p>
-        </div>,
-        {
-            // autoClose: 5000,
-            // hideProgressBar: false,
-            // closeOnClick: true,
-            // pauseOnHover: true,
-            // draggable: true,
-            // progress: undefined,
-            // theme: isDarkTheme ? 'dark' : 'light',
-            ...props,
-            theme: isDarkTheme ? 'dark' : 'light',
-        },
-    );
+    let toastFuncs2;
+
+    switch (toastType) {
+        case 'info':
+        case 'success':
+        case 'warning':
+        case 'error':
+            toastFuncs2 = toast[toastType]('meow meow');
+            break;
+
+        default:
+            toastFuncs2 = toast('nahnahnah');
+    }
+
+    // const toastFuncs = haveImage && props.srcImg ? toast[type] : toast;
+    // const toastFuncs = toast['info']('meow');
+
+    // const message = (
+    //     <div>
+    //         {title && <h3>{title}</h3>}
+    //         {content && <p>{content}</p>}
+    //         {haveImage && props.srcImg && <img src={props.srcImg} alt="" />}
+    //     </div>
+    // );
+
+    // console.log(toastFuncs);
+
+    // toastFuncs(message, {
+    //     ...props,
+    //     theme: isDarkTheme ? 'dark' : 'light',
+    // });
+
+    // toast.info(
+    //     <div>
+    //         {haveImage && <img src={props.srcImg} alt="Oops!!!" style={{ maxWidth: '100%', maxHeight: '250px' }} />}
+    //         <h3>{title}</h3>
+    //         <p>{content}</p>
+    //     </div>,
+    //     {
+    //         // autoClose: 5000,
+    //         // hideProgressBar: false,
+    //         // closeOnClick: true,
+    //         // pauseOnHover: true,
+    //         // draggable: true,
+    //         // progress: undefined,
+    //         // theme: isDarkTheme ? 'dark' : 'light',
+    //         ...props,
+    //         theme: isDarkTheme ? 'dark' : 'light',
+    //     },
+    // );
 };
