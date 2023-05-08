a58d50bd6ad8735b9a45276b90c5ee64fc269d1d
Ha Duc <haducvcvb@gmail.com>
2023-05-08 13:33:18 +0700

    about-me__utils already for use

--- Projects/website_about_me/about_me/src/components/work/WorkItems.jsx
+++ Projects/website_about_me/about_me/src/components/work/WorkItems.jsx
@@ -6,36 +6,6 @@ import { handleNotify } from '../../utils';
 const WorkItems = ({ item }) => {
     const [hovered, setHovered] = useState(false);
 
-    //  handle when clicked
-    const whenClickedToDemo = (e) => {
-        // bcs dont have pages preview so i'll show them the message (toast message)
-
-        // first remove preventDefault() of a card
-        e.preventDefault();
-
-        const isDarkTheme = getComputedStyle(document.body).getPropertyValue('--body-color') === '#202124';
-
-        toast.info(
-            <div>
-                <h3>Thông báo</h3>
-                {/* <p>Tính năng đang được phát triển. Vui lòng thử lại sau! ❤️</p> */}
-                <img
-                    src="https://scontent.fhan2-5.fna.fbcdn.net/v/t39.30808-6/344803825_259905209786528_2805273012473307820_n.jpg?stp=dst-jpg_p843x403&_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=NtBVZuCqrVQAX823QPx&_nc_ht=scontent.fhan2-5.fna&oh=00_AfCoHz9NSX881wEGKPmBGpFQQEK01tYvoSLCguBlhEjM1Q&oe=645C260D"
-                    alt=""
-                />
-            </div>,
-            {
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: isDarkTheme ? 'dark' : 'light',
-            },
-        );
-    };
-
     return (
         <div className="work__card" key={item.id}>
             <img
@@ -47,16 +17,14 @@ const WorkItems = ({ item }) => {
             />
             <h3 className="work__title">{item.title}</h3>
 
-            {/* <a href="#" className="work__button" onClick={(e) => whenClickedToDemo(e)}> */}
             <a
                 href="#"
                 className="work__button"
                 onClick={(e) =>
                     handleNotify(e, {
-                        haveImage: false,
-                        srcImg: 'https://scontent.fhan2-5.fna.fbcdn.net/v/t39.30808-6/344803825_259905209786528_2805273012473307820_n.jpg?stp=dst-jpg_p843x403&_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=NtBVZuCqrVQAX823QPx&_nc_ht=scontent.fhan2-5.fna&oh=00_AfCoHz9NSX881wEGKPmBGpFQQEK01tYvoSLCguBlhEjM1Q&oe=645C260D',
                         title: 'Thông báo',
                         content: 'Tính năng đang được phát triển. Vui lòng thử lại sau! ❤️',
+                        toastType: 'info',
                     })
                 }
             >
--- Projects/website_about_me/about_me/src/utils.js
+++ Projects/website_about_me/about_me/src/utils.js
@@ -5,7 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // handle when call the notify
-export const handleNotify = (e, { haveImage = false, title, content, toastType = 'error2', ...props }) => {
+export const handleNotify = (e, { haveImage = false, title, content, toastType = 'info2', ...props }) => {
     // bcs dont have pages preview so i'll show them the message (toast message)
 
     // first remove preventDefault() of a card
@@ -36,9 +36,9 @@ export const handleNotify = (e, { haveImage = false, title, content, toastType =
         <div>
             <h3>{title}</h3>
             <p>{content}</p>
-            <form>
+            {/* <form>
                 <input type="text" />
-            </form>
+            </form> */}
             {haveImage && props.srcImg && (
                 <div
                     style={{
@@ -46,7 +46,7 @@ export const handleNotify = (e, { haveImage = false, title, content, toastType =
                         alignItems: 'center',
                         justifyContent: 'center',
                         paddingTop: '6%',
-                        paddingLeft: '6%',
+                        paddingLeft: '5%',
                     }}
                 >
                     <img
@@ -62,7 +62,7 @@ export const handleNotify = (e, { haveImage = false, title, content, toastType =
     // handle options of toast
     const options = {
         // default
-        autoClose: 2000,
+        autoClose: 2500,
         hideProgressBar: false,
         closeOnClick: false,
         pauseOnHover: true,
