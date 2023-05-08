43875505da8e80ac1234c89b2056338917f54eff
Ha Duc <haducvcvb@gmail.com>
2023-05-08 09:00:21 +0700

    about-me_upgrade #2

--- Projects/website_about_me/about_me/src/utils.js
+++ Projects/website_about_me/about_me/src/utils.js
@@ -5,7 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // handle when call the notify
-export const handleNotify = (e, { haveImage = false, title, content, ...props }) => {
+export const handleNotify = (e, { haveImage = false, title, content, type = 'toast', ...props }) => {
     // bcs dont have pages preview so i'll show them the message (toast message)
 
     // first remove preventDefault() of a card
@@ -14,27 +14,23 @@ export const handleNotify = (e, { haveImage = false, title, content, ...props })
     // handle for dark theme & light theme
     const isDarkTheme = getComputedStyle(document.body).getPropertyValue('--body-color') === '#202124';
 
+    const toastFuncs = haveImage && props.srcImg ? toast[type] : toast;
+
     toast.info(
         <div>
-            {/* <h3>Thông báo</h3> */}
-            {/* <p>Tính năng đang được phát triển. Vui lòng thử lại sau! ❤️</p> */}
-            {/* {haveImage && (
-                <img
-                    src="https://scontent.fhan2-5.fna.fbcdn.net/v/t39.30808-6/344803825_259905209786528_2805273012473307820_n.jpg?stp=dst-jpg_p843x403&_nc_cat=109&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=NtBVZuCqrVQAX823QPx&_nc_ht=scontent.fhan2-5.fna&oh=00_AfCoHz9NSX881wEGKPmBGpFQQEK01tYvoSLCguBlhEjM1Q&oe=645C260D"
-                    alt=""
-                />
-            )} */}
-            {haveImage && <img src={props.srcImg} alt="" />}
+            {haveImage && <img src={props.srcImg} alt="Oops!!!" style={{ maxWidth: '100%', maxHeight: '250px' }} />}
             <h3>{title}</h3>
             <p>{content}</p>
         </div>,
         {
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
+            // autoClose: 5000,
+            // hideProgressBar: false,
+            // closeOnClick: true,
+            // pauseOnHover: true,
+            // draggable: true,
+            // progress: undefined,
+            // theme: isDarkTheme ? 'dark' : 'light',
+            ...props,
             theme: isDarkTheme ? 'dark' : 'light',
         },
     );
