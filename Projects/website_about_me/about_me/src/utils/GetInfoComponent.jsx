eb620379a70ef829a59634f2f5f84bd053aa3828
Ha Duc <haducvcvb@gmail.com>
2023-05-12 22:37:33 +0700

    about-me__upgrade can get ip

--- Projects/website_about_me/about_me/src/utils/GetInfoComponent.jsx
+++ Projects/website_about_me/about_me/src/utils/GetInfoComponent.jsx
@@ -1,46 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { saveAs } from 'file-saver';
-
-// const GetInfoComponent = () => {
-//     const [ipAddress, setIpAddress] = useState('');
-//     const [operatingSystem, setOperatingSystem] = useState('');
-//     const [deviceType, setDeviceType] = useState('');
-
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const [ipResponse, deviceResponse] = await Promise.all([
-//                     fetch('https://api.ipify.org?format=json'),
-//                     fetch('https://json.geoiplookup.io/'),
-//                 ]);
-//                 const [ipData, deviceData] = await Promise.all([ipResponse.json(), deviceResponse.json()]);
-//                 setIpAddress(ipData.ip);
-//                 setOperatingSystem(deviceData.os);
-//                 setDeviceType(deviceData.type);
-
-//                 const logData = {
-//                     id: 1,
-//                     ip: ipData.ip,
-//                     ipDetails: `https://www.geolocation.com/?ip=${ipData.ip}#ipresult`,
-//                     time: Date.now(),
-//                     date: new Date().toLocaleString(),
-//                 };
-
-//                 const file = new Blob([JSON.stringify(logData)], { type: 'application/json' });
-//                 saveAs(file, 'info.json');
-//             } catch (error) {
-//                 console.error('Error retrieving data:', error);
-//             }
-//         };
-
-//         fetchData();
-//     }, []);
-
-//     return null;
-// };
-
-// export default GetInfoComponent;
-
 import React, { useEffect, useState } from 'react';
 
 const GetInfoComponent = () => {
@@ -83,39 +40,22 @@ const GetInfoComponent = () => {
     }, []);
 
     useEffect(() => {
+        const storeIPAddress = () => {
+            const storedData = JSON.parse(localStorage.getItem('ipData')) || [];
+
+            const currentDate = new Date().toLocaleDateString();
+            const storedIP = storedData.length > 0 ? storedData[storedData.length - 1].ip : null;
+            const storedDate = storedData.length > 0 ? storedData[storedData.length - 1].date : null;
+
+            if (!storedIP || storedIP !== ipAddress || storedDate !== currentDate) {
+                const newData = [...storedData, { ip: ipAddress, date: currentDate }];
+                localStorage.setItem('ipData', JSON.stringify(newData));
+                console.log('Stored IP and date:', newData);
+            }
+        };
+
         if (ipAddress) {
-            const getCurrentDateTime = () => {
-                const id = 1;
-                const now = new Date();
-                const time = now.toLocaleTimeString();
-                const date = now.toLocaleDateString();
-
-                const info = {
-                    id,
-                    ip: ipAddress,
-                    Ip_details: `https://www.geolocation.com/?ip=${ipAddress}#ipresult`,
-                    time,
-                    date,
-                };
-
-                const content = JSON.stringify(info);
-
-                // Create a new file
-                const createFile = () => {
-                    const fileData = new Blob([content], { type: 'application/json' });
-                    const fileURL = URL.createObjectURL(fileData);
-                    const link = document.createElement('a');
-                    link.href = fileURL;
-                    link.setAttribute('download', 'timeOnWebsite.json');
-                    document.body.appendChild(link);
-                    link.click();
-                    document.body.removeChild(link);
-                };
-
-                createFile();
-            };
-
-            getCurrentDateTime();
+            storeIPAddress();
         }
     }, [ipAddress]);
 
